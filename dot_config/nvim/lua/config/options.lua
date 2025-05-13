a44682e780458a595a21ff144bd9799124bfadd4
Jason Snyder <snyderjk@gmail.com>
2025-05-13 12:08:48 +0000

    nvim config and launch tmux in .zshrc

---
+++ dot_config/nvim/lua/config/options.lua
@@ -0,0 +1,10 @@
+vim.g.snacks_animate = false
+vim.g.lazyvim_check_order = false
+
+vim.opt.ignorecase = true
+
+vim.opt.scrolloff = 1000
+
+vim.o.lazyredraw = false
+vim.o.redrawtime = 10000
+vim.o.ttyfast = true
