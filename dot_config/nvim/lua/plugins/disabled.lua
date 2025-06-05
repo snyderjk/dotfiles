32b38be8829a6ae84866e61e9dcf9939650267e8
Jason Snyder <snyderjk@gmail.com>
2025-06-05 17:14:34 +0000

    changing lazy explorer

--- dot_config/nvim/lua/plugins/disabled.lua
+++ dot_config/nvim/lua/plugins/disabled.lua
@@ -1,4 +1,10 @@
 return {
 	{ "folke/noice.nvim", enabled = false },
 	{ "rcarriga/nvim-notify", enabled = false },
+	{
+		"folke/snacks.nvim",
+		opts = {
+			explorer = { enabled = false },
+		},
+	},
 }
